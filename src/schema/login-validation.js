49b022acbd9b36239b0404106322243e23641ad1
gdeananthap <gdeanantha33@gmail.com>
2023-04-07 21:47:16 +0700

    fix: add string max value on login validation

--- src/schema/login-validation.js
+++ src/schema/login-validation.js
@@ -4,9 +4,11 @@ export const loginValidation = yup.object({
   email: yup
     .string('Enter your email')
     .email('Enter a valid email')
-    .required('Please fill in the required field'),
+    .required('Please fill in the required field')
+    .max(255, 'Email is too long'),
   password: yup
     .string('Enter your password')
     .min(8, 'Password should be of minimum 8 characters length')
-    .required('Please fill in the required field'),
+    .required('Please fill in the required field')
+    .max(255, 'Password is too long'),
 });
