b9f064d531e6118a006abdcd1bbbd0be55b7a737
gdeananthap <gdeanantha33@gmail.com>
2023-04-21 20:19:42 +0700

    feat: add delete repo dialog

--- src/pages/repository/setting/general.js
+++ src/pages/repository/setting/general.js
@@ -4,18 +4,20 @@ import { useRouter } from 'next/router';
 import { useTheme } from '@mui/material/styles';
 import { Container, Box, Typography, Button, IconButton} from '@mui/material';
 import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import CloseIcon from '@mui/icons-material/Close';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import FolderSharedOutlinedIcon from '@mui/icons-material/FolderSharedOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import NavBar from '@/component/navbar';
 import Loading from '@/component/loading';
 import SettingRepositoryTabs from '@/component/tabs/setting-repository';
 import FormInput from '@/component/form-input';
 import { editRepositoryValidation } from '@/schema/edit-repository';
+import { deleteRepositoryValidation } from '@/schema/delete-repository';
+import FormInputDialog from '@/component/form-input-dialog';
+import CustomAlert from '@/component/custom-alert';
 
 export default function GeneralSettingRepository() {
   const theme = useTheme();
@@ -26,6 +28,8 @@ export default function GeneralSettingRepository() {
   const [visibility, setVisibility] = useState('public');
   const [isVisibilityModalOpen, setIsVisibilityModalOpen] = useState(false);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const repositoryName = 'Repository XYZ'
 
   const hasFormChanged = (currentValues, initialValues) => Object.keys(initialValues).some(fieldName => initialValues[fieldName] !== currentValues[fieldName]);
   
@@ -41,13 +45,27 @@ export default function GeneralSettingRepository() {
 
   const formik = useFormik({
     initialValues: {
-      name: 'Repository XYZ',
+      name: repositoryName,
       description: '',
     },
     validationSchema: editRepositoryValidation,
     onSubmit: (values) => {
       setIsLoading(true);
       console.log(values);
+      setShowAlert(true);
+      setIsLoading(false);
+    } ,
+  });
+
+  const formikDialog = useFormik({
+    initialValues: {
+      name: '',
+      real_name: repositoryName,
+    },
+    validationSchema: deleteRepositoryValidation,
+    onSubmit: (values) => {
+      setIsLoading(true);
+      router.push({ pathname: '/', query: { deleteRepository : values.name }})
       setIsLoading(false);
     } ,
   });
@@ -65,6 +83,8 @@ export default function GeneralSettingRepository() {
     handleCloseVisibility();
   }
 
+  const handleClickShowAlert= () => setShowAlert((show) => !show);
+
   return (
     <>
       { isLoading && <Loading centered={true}/> }
@@ -73,6 +93,13 @@ export default function GeneralSettingRepository() {
           <NavBar 
             setIsLoading={setIsLoading}
           />
+          { !isLoading && showAlert &&
+            <CustomAlert
+              severity={'success'}
+              label={'Your changes to the repository settings have been successfully saved'}
+              onClose={handleClickShowAlert}
+            /> 
+          }
           <Container 
             sx={{
               padding: '40px', 
@@ -321,6 +348,7 @@ export default function GeneralSettingRepository() {
                         typography: theme.typography.heading_h6,
                         color: theme.palette.white.main,
                       }}
+                      onClick={handleClickOpenDelete}
                     >
                       Delete This Repository
                     </Button> 
@@ -407,6 +435,114 @@ export default function GeneralSettingRepository() {
               >
                 {visibility === 'public'? 'Make this repository private' : 'Make this repository public'}
               </Button> 
+            </DialogContent>
+          </Dialog>
+          <Dialog
+            open={isDeleteModalOpen}
+            onClose={handleCloseDelete}
+            sx={{
+              "& .MuiDialog-container": {
+                "& .MuiPaper-root": {
+                  width: "100%",
+                  maxWidth: "516px",
+                },
+              },
+            }}
+          >
+            <DialogTitle id="alert-dialog-title"
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+                padding: '24px 24px 40px 24px'
+              }}
+            >
+              <Typography variant='popup_heading' color='black.main'>Delete this repository</Typography>
+              <IconButton sx={{ padding: 0, }} onClick={handleCloseDelete}>
+                <CloseIcon
+                  sx={{
+                    width: '36px',
+                    height: '36px',
+                    color: theme.palette.dark_gray.main,
+                  }}
+                />
+              </IconButton>
+            </DialogTitle>
+            <DialogContent
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+                padding: '16px 24px 24px 24px',
+                gap: '12px',
+              }}
+            >
+              <DeleteOutlinedIcon
+                sx={{
+                  width: '64px',
+                  height: '64px',
+                  color: theme.palette.dark_gray.main
+                }}
+              /> 
+              <Typography variant='form_label_small' color='black.main' textAlign='center'>{`Delete ${repositoryName}`}</Typography>
+              <Typography 
+                variant='form_sublabel_small' 
+                color='black.main'
+                textAlign='center'
+                sx={{
+                  "& .bold": {
+                    typography: theme.typography.form_sublabel_small_bold
+                  }
+                }}
+              >
+                This action <span className='bold'>cannot</span> be undone. This will permanently delete the <span className='bold'>{repositoryName}</span>, delete all <span className='bold'>documents</span> inside and remove all <span className='bold'>collaborator associations.</span>
+              </Typography>
+              <form 
+                onSubmit={formikDialog.handleSubmit} 
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  width: '100%',
+                  gap: '24px',
+                  marginTop: '12px',
+                }}
+              >
+                <FormInputDialog
+                  id='name'            
+                  name='name'
+                  label={["Please type ", <span key={1} className='bold'>{repositoryName}</span>,  " to confirm."]}
+                  placeholder={repositoryName}
+                  value={formikDialog.values.name}
+                  onChange={formikDialog.handleChange}
+                  onBlur={formikDialog.handleBlur}
+                  error={formikDialog.touched.name && Boolean(formikDialog.errors.name)}
+                  helpertext={formikDialog.touched.name && formikDialog.errors.name}
+                />
+                <Button 
+                  color='danger_button' 
+                  variant='contained' 
+                  sx={{ 
+                    height: '32px', 
+                    padding: '0 12px',
+                    width: '100%',
+                    marginTop: '4px',
+                    typography: theme.typography.heading_h6,
+                    color: theme.palette.white.main,
+                    '&.Mui-disabled': {
+                      backgroundColor: theme.palette.dark_gray.light,
+                      color: theme.palette.white.main,
+                    },
+                  }}
+                  type="submit"
+                  disabled={!formikDialog.touched.name || Boolean(formikDialog.errors.name)}
+                >
+                  I understand, delete this repository
+                </Button>
+              </form>
             </DialogContent>
           </Dialog>
         </>
