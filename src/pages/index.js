f72eaeaea25ae4329467d18f072556f3f81c9049
gdeananthap <gdeanantha33@gmail.com>
2023-04-21 15:00:32 +0700

    fix: fix handleSearch function

--- src/pages/index.js
+++ src/pages/index.js
@@ -244,7 +244,7 @@ export default function Home() {
                     backgroundColor: theme.palette.primary.main,
                     borderRadius: '5px'
                   }}
-                  onClick={handleSearch}
+                  onClick={() => handleSearch()}
                 >
                   <SearchIcon />
                 </Button>
