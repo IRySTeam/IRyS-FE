5a28128ffa2054f2747ab45cc1af77d30426cd6c
gdeananthap <gdeanantha33@gmail.com>
2023-05-21 13:27:38 +0700

    add features

---
+++ src/data/filter.js
@@ -0,0 +1,128 @@
+export const generalFilter = [
+  {
+    value: 'PER',
+    desc: 'Entity that denotes a person`s name',
+  },
+  {
+    value: 'ORG',
+    desc: 'Entity that denotes an organization',
+  },
+  {
+    value: 'LOC',
+    desc: 'Entity that denotes a name of a place or location',
+  },
+  {
+    value: 'MISC',
+    desc: 'Other detected entities',
+  },
+  {
+    value: 'Mimetype',
+    desc: 'Document format',
+  },
+  {
+    value: 'Extension',
+    desc: 'Document extension',
+  },
+  {
+    value: 'Size',
+    desc: 'Document size in bytes',
+  },
+  {
+    value: 'Dates',
+    desc: 'Dates that are mentioned within the content of the document',
+  },
+]
+
+export const filterRecruitment = [
+  {
+    value: 'COMPANY',
+    desc: 'Entity that denotes the name of a company that a person has been affiliated with',
+  },
+  {
+    value: 'INSTITUTION',
+    desc: 'Entity that denotes the name of an institution that a person has been affiliated with',
+  },
+  {
+    value: 'DEGREE',
+    desc: 'Entity that denotes the degree a person has obtained',
+  },
+  {
+    value: 'ROLE',
+    desc: 'Entity that denotes a job related role that a person has experience in',
+  },
+  {
+    value: 'SKILL',
+    desc: 'Entity that denotes skills that a person has',
+  },
+  {
+    value: 'Name',
+    desc: 'Name of the resume or CV author',
+  },
+  {
+    value: 'Skills',
+    desc: 'Skills that belong to the CV or resume author',
+  },
+  {
+    value: 'Email',
+    desc: 'Email of the resume or CV author ',
+  },
+  {
+    value: 'Experiences',
+    desc: 'Job related experiences that belong to the CV or resume author',
+  },
+  {
+    value: 'Education',
+    desc: 'Education history that belong to the CV or resume author',
+  },
+  {
+    value: 'Projects',
+    desc: 'Projects that have been done by the CV or resume author',
+  },
+  {
+    value: 'Certifications',
+    desc: 'Certifications that have been obtained by the CV or resume author',
+  },
+]
+
+export const filterScientific= [
+  {
+    value: 'CONCEPT',
+    desc: 'Entity that denotes a concept in a scientific document',
+  },
+  {
+    value: 'INSTITUTION',
+    desc: 'Entity that denotes the name of an institution that a scientific document has been affiliated with',
+  },
+  {
+    value: 'TOOLS',
+    desc: 'Entity that denotes tools mentioned in a scientific document',
+  },
+  {
+    value: 'METRICS',
+    desc: 'Entity that denotes metrics mentioned in a scientific document',
+  },
+  {
+    value: 'Title',
+    desc: 'Title of the document',
+  },
+  {
+    value: 'Authors',
+    desc: 'Authors mentioned within the document',
+  },
+  {
+    value: 'Affiliations',
+    desc: 'Organizations or institutions that are mentioned to be affiliated with the document',
+  },
+  {
+    value: 'Abstract',
+    desc: 'Abstract of the document',
+  },
+  {
+    value: 'Keywords',
+    desc: 'Keywords mentioned within the document',
+  },
+  {
+    value: 'References',
+    desc: 'References used by the document',
+  },
+]
---
+++ src/data/top_level.js
@@ -0,0 +1,14 @@
+export const topLevelQueryParams = [
+  {
+    label: 'domain',
+    desc: 'Represents the domain related scope of the search, current available values include general, recruitment, and scientific.',
+  },
+  {
+    label: 'query',
+    desc: 'Keyword or phrase-based search that will be used as the main body of the semantic search.',
+  },
+  {
+    label: 'advanced_filter',
+    desc: 'A list of key-value pairs that represent domain specific entities to be searched for within documents. The entity values present in the document and the one specified within the query will be evaluated based on the specified operator. ',
+  },
+]
